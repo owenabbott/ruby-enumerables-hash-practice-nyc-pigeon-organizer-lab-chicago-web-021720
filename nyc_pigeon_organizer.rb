9e97a255803563572a249900e94bffb70b082e51
Owen Abbott <ocabbott@gmail.com>
2020-02-06 07:47:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -61,7 +61,7 @@ end
 pp pidgeon_colors
 
 for a in pidgeon_names
-  pidgy[a][:color]=pidgeon_colors[pidgeon_names.index(a)]
+  pidgy[a][:color]=pidgeon_colors[pidgeon_names.index(a)].to_s
 end
 pp pidgy
 return pidgy
