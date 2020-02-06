bf5214c10e54145e812d98715f93d1a70e9bdd5b
Owen Abbott <ocabbott@gmail.com>
2020-02-06 06:34:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,27 @@ def nyc_pigeon_organizer(data)
 #  pp data
   pidgy = Hash.new
   data.each do |key, value|
-    for i in key
-    pidgy[i]={}
+    if key == :color 
+      key.each do |color, name|
+        pidgy[name]=>[].push(color)
+      end
+    end
   end
-end
 pp pidgy
   
 end
+
+
+
+
+
+#saving my tentatively working code in comment mode so I can  copy and paste it if I destroy it later as I edit:
+
+
+#  pidgy = Hash.new
+#  data.each do |key, value|
+#    for i in key
+#    pidgy[i]={}
+#  end
+#end
+#pp pidgy
